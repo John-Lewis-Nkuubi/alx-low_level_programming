f687952fc7b4e0ae257243f541a0f36fbf324e78
John Lewis Nkuubi <nlewisjohn@gmail.com>
2023-12-16 01:26:05 +0300

    print text using function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that prints a text with a new line using puts fucntion
  * Return: 0 (success)
  */
-int main(volid)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
