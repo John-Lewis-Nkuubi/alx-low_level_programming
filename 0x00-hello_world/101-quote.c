81a9cf80fd68d029275156d357d5b0395d5b680e
John Lewis Nkuubi <nlewisjohn@gmail.com>
2023-12-16 03:19:21 +0300

    print without man (3) printf or man (3) puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A program that prints text minus use of  man (3) printf or man (3) puts
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
