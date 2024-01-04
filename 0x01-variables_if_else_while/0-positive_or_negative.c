ade4286ded1bf8442c6d9a1814086d98e0ae5f65
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-04 10:56:29 +0300

    assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main - Entry point
+/**
+ * main - Program will assign a random number to the variable n
  *
  * Return - 0 (success)
  */
