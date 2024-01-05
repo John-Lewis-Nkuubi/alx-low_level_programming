e272b63bfc698c0af0a5541675bea2d5a86f5675
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-06 00:21:41 +0300

    print alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints lowercase of the aphabet
+ * return - (0) success
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	if(n <= 'z')
+	{
+		putchar (n);
+		putchar ('\n');
+	}
+	return (0);
+}
