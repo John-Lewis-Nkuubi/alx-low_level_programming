b62009a24cd9988e1d6f8ebf62cc92002919067c
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-06 02:05:32 +0300

    prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers of base 10
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int q;
+
+	for (q = 0; q < 10; q++)
+		putchar (q);
+	putchar ('\n');
+	return (0);
+}
