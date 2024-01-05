924e3a1d76dc6dc2018265edcd2ef64a130a649f
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-06 01:51:34 +0300

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all lowercase letters except q and e
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i != 'q' && i != 'e'; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+}
