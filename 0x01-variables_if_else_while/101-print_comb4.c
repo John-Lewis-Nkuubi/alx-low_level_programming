4752ed273213ff74e5191e8bf41ace3ac2855286
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-08 02:29:55 +0300

    print all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible
+ * different combinations of three digits.
+ * Descripton - Numbers are separated by comma
+ * followed by a space
+ * The three digits are different
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * Of the three digits 0, 1 and 2
+ * The smallest combination of three digits is printed
+ * Numbers are printed in ascending order, with three digits
+ * only use the putchar function is in us
+ * putchar is in use at a  six times maximum
+ * no variable of type char is in use
+ * All the code is in the main function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	return (0);
+}
