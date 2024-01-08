a35cc274c1875fdc5aea4869c760c68c8e28055a
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-08 03:34:12 +0300

    print three digit combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,5 +19,30 @@
 
 int main(void)
 {
+	int o;
+	int t;
+	int h;
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		for (t = '0'; t <= '9'; t++)
+		{
+			for (o = '0'; o <= '9'; o++)
+			{
+				if (!(((o == t) && (t == h)) || ((h > t) && (t > o))))
+				{
+					putchar(h);
+					putchar(t);
+					putchar(o);
+					if (!(o == '9' && t == '8' && h == '7'))
+					{
+						putchar(',');
+						putchar(' ');
+					}					
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
