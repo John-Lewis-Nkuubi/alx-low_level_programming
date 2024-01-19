318c7d93430e2c99541c39918eb147cb060a76d2
John Lewis Nkuubi <nlewisjohn@gmail.com>
2024-01-19 13:11:07 +0300

    fibonacci sum of evenupto 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - sum up even fibonacci upto 4 million
+ * Return: (0)
+ */
+
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0, h = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		if ((y % 2 == 0) && (y <= 4000000))
+		{
+			h += y;
+		}
+		sum = x + y;
+		x = y;
+		y = sum;
+	}
+	return (0);
+}
