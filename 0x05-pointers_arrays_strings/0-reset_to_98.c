0be2d15b6cf5c21d14e973463889314a0e5b1818
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-01-26 02:22:36 +0300

    reset the value of an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - a program that resets the value of 
+ * an int to 98
+ * @n: the value of a dereferencing pointer
+ * Return: nth
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
