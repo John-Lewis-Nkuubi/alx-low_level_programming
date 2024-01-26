eccb9ac84ac65ad7c584aa9879a71f878f8eb676
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-01-26 12:33:23 +0300

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - print string followed by new line
+ *
+ * @str: prints string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[1] != '\0'; i++)
+	{
+		_putchar(_str[i]);
+	}
+	_putchar('\n');
+}
