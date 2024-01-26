917adc2b0f7ec9633e208076cd930a134b942d96
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-01-26 22:51:07 +0300

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (*s(s + count) != '\0')
+	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
@@ -21,14 +21,14 @@ int _atoi(char *s)
 		{
 			if (size > 0)
 				m *= 10;
-			size++
+			size++;
 		}
 		count++;
 	}
 
 	for (i = count - size; i < count; i++)
 	{
-		oi = oi + ((*(s + i) = 48) * m);
+		oi = oi + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
 	return (oi * pn);
