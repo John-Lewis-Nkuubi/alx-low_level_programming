20c5117da9201cc31fbf770bcae333e26700a22e
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-01-26 15:27:20 +0300

    copy the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * *_strcpy - a function that copies the string pointed to by src, 
- * including the terminating null byte (\0), to 
+ * *_strcpy - a function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to
  * the buffer pointed to by dest.
  *
  * @dest: string 1 pointer to a char
