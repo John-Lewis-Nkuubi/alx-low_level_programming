37e548d41d1f47bdc43a9fe1f748ccee5a3e9bef
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-02-20 15:48:24 +0300

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concatenate strings, defining the size of second string
+ * *dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, t;
+
+	i = 0;
+	t = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + t) != '\0' && t < 97 && t < n)
+	{
+		*(dest + t) = *(src + t);
+		i++;
+		t++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
