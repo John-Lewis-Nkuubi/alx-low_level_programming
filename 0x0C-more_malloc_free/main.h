b096e2266156da9ca5ae521b89de75efd6e26585
John-Lewis-Nkuubi <nlewisjohn@gmail.com>
2024-07-08 14:01:08 +0300

    header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
-char *create_array(unsigned int size, char c);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int len(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* MAIN_H */
+#endif /*MAIN_H*/
